da64a947509558b8a032eab903697af7edb4ea2c
aysar <aysarhajaj@gmail.com>
2023-02-23 04:48:56 +0200

    add routes for login, logout, and register

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::middleware('auth:api')->get('logout', [AuthenticationController::class, 'logout']);
