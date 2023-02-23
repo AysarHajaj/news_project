181b8ea8bc9fa636acefc301b430f82df84145e2
aysar <aysarhajaj@gmail.com>
2023-02-23 17:35:44 +0200

    add getData function

--- backend/app/Http/Controllers/DataController.php
+++ backend/app/Http/Controllers/DataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GetDataRequest;
 use App\Services\DataService;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,26 @@ class DataController extends Controller
     public function __construct(DataService $dataService)
     {
         $this->dataService = $dataService;
+    }
+
+    public function getData(GetDataRequest $request)
+    {
+        try {
+            $input = $request->only([
+                'search_text',
+                'category',
+                'sources',
+                'from_date',
+                'to_date',
+                'page'
+            ]);
+
+            $response = $this->dataService->getData($input);
+
+            return response()->json($response, 200);
+        } catch (\Throwable $th) {
+            $response = ["error" => $th->getMessage()];
+            return response()->json($response, 500);
+        }
     }
 }
