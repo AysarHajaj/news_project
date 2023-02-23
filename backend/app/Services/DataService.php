65a515396ff7b34ca473b7331c4a870cdfd361e4
aysar <aysarhajaj@gmail.com>
2023-02-23 18:04:23 +0200

    implement getData function

--- backend/app/Services/DataService.php
+++ backend/app/Services/DataService.php
@@ -2,6 +2,17 @@
 
 namespace App\Services;
 
+use App\Classes\Apis\GuardianApi;
+use App\Classes\Apis\NewsApi;
+use App\Classes\Apis\NewYorkTimesApi;
+use App\Classes\Formatters\GuardianFormatter;
+use App\Classes\Formatters\NewsFormatter;
+use App\Classes\Formatters\NewYorkTimesFormatter;
+use App\Classes\QueryBuilders\GuardianQueryBuilder;
+use App\Classes\QueryBuilders\NewsQueryBuilder;
+use App\Classes\QueryBuilders\NewYorkTimesQueryBuilder;
+use App\Constants\Sources;
+
 class DataService
 {
     public function getData($input)
@@ -10,6 +21,31 @@ class DataService
             "articles" => [],
             "pages" => 0
         ];
+
+        foreach ($input['sources'] as $source) {
+            if ($source == Sources::NEWS_API) {
+                $api = new NewsApi(
+                    new NewsFormatter,
+                    new NewsQueryBuilder
+                );
+
+                $result = $this->exec($result, $api, $input);
+            }
+            if ($source == Sources::GUARDIAN_API) {
+                $api = new GuardianApi(
+                    new GuardianFormatter,
+                    new GuardianQueryBuilder
+                );
+                $result = $this->exec($result, $api, $input);
+            }
+            if ($source == Sources::NEW_YORK_TIMES_API) {
+                $api = new NewYorkTimesApi(
+                    new NewYorkTimesFormatter,
+                    new NewYorkTimesQueryBuilder
+                );
+                $result = $this->exec($result, $api, $input);
+            }
+        }
 
         return $result;
     }
