991f50ca820ef86f1ddbaca6fed2672559e50ce9
aysar <aysarhajaj@gmail.com>
2023-02-23 04:39:56 +0200

    add addSettingForRegisteredUser and insertSettingSources functions

--- backend/app/Services/SettingService.php
+++ backend/app/Services/SettingService.php
@@ -2,6 +2,34 @@
 
 namespace App\Services;
 
+use App\Constants\Sources;
+use App\Models\Setting;
+use App\Models\SettingSource;
+
 class SettingService
 {
+    public function addSettingForRegisteredUser($userId)
+    {
+        $setting = Setting::create([
+            'user_id' => $userId
+        ]);
+
+        $this->insertSettingSources(
+            array_column(Sources::ALL, 'id'),
+            $setting->id
+        );
+    }
+
+    public function insertSettingSources($sources, $settingId)
+    {
+        $settingSourcesData = [];
+        foreach ($sources as $source) {
+            $settingSourcesData[] = [
+                'setting_id' => $settingId,
+                'source' => $source
+            ];
+        }
+
+        SettingSource::insert($settingSourcesData);
+    }
 }
