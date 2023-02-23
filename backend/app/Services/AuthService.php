16a832a41ce4a1230b874cf2f6b51614fd5f2d24
aysar <aysarhajaj@gmail.com>
2023-02-23 04:33:49 +0200

    add register function

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,6 +2,32 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
 class AuthService
 {
+    public function register($input)
+    {
+        DB::beginTransaction();
+        try {
+            $input['password'] = bcrypt($input['password']);
+
+            $user = User::create($input);
+
+            $response = [
+                "result" => [
+                    "user" => $user,
+                    "token" => $user->createToken('innoscripta')->accessToken
+                ]
+            ];
+
+            DB::commit();
+            return $response;
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw new Exception($th->getMessage());
+        }
+    }
 }
