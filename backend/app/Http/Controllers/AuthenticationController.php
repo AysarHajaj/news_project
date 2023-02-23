16b3e86406f1f81a47d819169d98614de3cc0a68
aysar <aysarhajaj@gmail.com>
2023-02-23 04:36:24 +0200

    inject auth service

--- backend/app/Http/Controllers/AuthenticationController.php
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistrationRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
+    private $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
     public function register(RegistrationRequest $request)
     {
         try {
             $input = $request->only(['name', 'email', 'password']);
-            $response = [];
+            $response = $this->authService->register($input);
 
             return response()->json($response, 200);
         } catch (\Throwable $th) {
