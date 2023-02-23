bbeac9c933f217468a64e586ba8253647722a629
aysar <aysarhajaj@gmail.com>
2023-02-23 04:31:10 +0200

    add register function

--- backend/app/Http/Controllers/AuthenticationController.php
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistrationRequest;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function register(RegistrationRequest $request)
+    {
+        try {
+            $input = $request->only(['name', 'email', 'password']);
+            $response = [];
+
+            return response()->json($response, 200);
+        } catch (\Throwable $th) {
+            $response = ["error" => $th->getMessage()];
+
+            return response()->json($response, 500);
+        }
+    }
 }
