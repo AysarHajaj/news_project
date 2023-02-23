7be6c1c671c5d09632d218051f46d6c77df98f8c
aysar <aysarhajaj@gmail.com>
2023-02-23 04:22:33 +0200

    implement LoginRequest functions

--- backend/app/Http/Requests/LoginRequest.php
+++ backend/app/Http/Requests/LoginRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
@@ -11,8 +14,19 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
+
+    public function messages()
+    {
+        return [
+            'email.required' => 'You need to enter your email',
+            'email.email' => 'The email should be email structured',
+            'password.required' => 'You need to enter a password',
+        ];
+    }
+
+
 
     /**
      * Get the validation rules that apply to the request.
@@ -22,7 +36,17 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "email" => "required|email",
+            "password" => "required",
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'error' => $validator->errors()->first(),
+        ], 400);
+
+        throw new ValidationException($validator, $response);
     }
 }
