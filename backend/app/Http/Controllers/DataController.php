892601a85e95c827f12cc4ee496308ed67e2b2f1
aysar <aysarhajaj@gmail.com>
2023-02-23 17:34:32 +0200

    inject data serivce

--- backend/app/Http/Controllers/DataController.php
+++ backend/app/Http/Controllers/DataController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\DataService;
 use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    //
+    private $dataService;
+
+    public function __construct(DataService $dataService)
+    {
+        $this->dataService = $dataService;
+    }
 }
