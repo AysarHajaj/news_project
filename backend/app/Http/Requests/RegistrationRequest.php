59c4712277dae7072023495c11d5000cead79461
aysar <aysarhajaj@gmail.com>
2023-02-23 04:21:03 +0200

    implement RegistrationRequest functions

--- backend/app/Http/Requests/RegistrationRequest.php
+++ backend/app/Http/Requests/RegistrationRequest.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class RegistrationRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+
+    public function messages()
     {
-        return false;
+        return [
+            'name.required' => 'You need to enter your name',
+            'email.required' => 'You need to enter your email',
+            'email.email' => 'The email should be email structured',
+            'email.unique' => 'The email you entered already exists',
+            'password.required' => 'You need to enter a password',
+            'password.confirmed' => 'The password confirmation should match you password',
+        ];
     }
+
+
 
     /**
      * Get the validation rules that apply to the request.
@@ -22,7 +32,18 @@ class RegistrationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|confirmed",
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'error' => $validator->errors()->first(),
+        ], 400);
+
+        throw new ValidationException($validator, $response);
     }
 }
