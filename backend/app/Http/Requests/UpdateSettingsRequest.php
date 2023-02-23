7da08ccf15420155b6fccd4cad1a0327f5fbe5c1
aysar <aysarhajaj@gmail.com>
2023-02-23 04:24:10 +0200

    implement UpdateSettingsRequest functions

--- backend/app/Http/Requests/UpdateSettingsRequest.php
+++ backend/app/Http/Requests/UpdateSettingsRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class UpdateSettingsRequest extends FormRequest
 {
@@ -11,8 +14,19 @@ class UpdateSettingsRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
+
+    public function messages()
+    {
+        return [
+            'sources.required' => 'You need to choose the data source',
+            'sources.array' => 'Sources must be array of strings',
+            'category.present' => 'The payload should contain category key'
+        ];
+    }
+
+
 
     /**
      * Get the validation rules that apply to the request.
@@ -22,7 +36,17 @@ class UpdateSettingsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'sources' => "required|array",
+            'category' => "present"
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new JsonResponse([
+            'error' => $validator->errors()->first(),
+        ], 400);
+
+        throw new ValidationException($validator, $response);
     }
 }
