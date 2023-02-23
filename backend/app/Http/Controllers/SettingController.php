84f71ec50b45815c97532959a8d34e03b9ad4b32
aysar <aysarhajaj@gmail.com>
2023-02-23 04:58:28 +0200

    add update function

--- backend/app/Http/Controllers/SettingController.php
+++ backend/app/Http/Controllers/SettingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateSettingsRequest;
 use App\Services\SettingService;
 
 class SettingController extends Controller
@@ -11,5 +12,19 @@ class SettingController extends Controller
     public function __construct(SettingService $settingService)
     {
         $this->settingService = $settingService;
+    }
+
+    public function update(UpdateSettingsRequest $request, $id)
+    {
+        try {
+            $input = $request->only(['sources', 'category']);
+            $response = $this->settingService->update($input, $id);
+
+            return response()->json($response, 200);
+        } catch (\Throwable $th) {
+            $response = ["error" => $th->getMessage()];
+
+            return response()->json($response, 500);
+        }
     }
 }
