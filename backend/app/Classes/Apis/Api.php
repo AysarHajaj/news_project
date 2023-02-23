8204c30b808bfce4297c06f0c0c9253603993e09
aysar <aysarhajaj@gmail.com>
2023-02-23 17:57:59 +0200

    add get, exec, and getCategoryIfExists functions

--- backend/app/Classes/Apis/Api.php
+++ backend/app/Classes/Apis/Api.php
@@ -4,6 +4,7 @@ namespace App\Classes\Apis;
 
 use App\Interfaces\FormatterInterface;
 use App\Interfaces\QueryBuilderInterface;
+use GuzzleHttp\Client;
 
 class Api
 {
@@ -16,5 +17,36 @@ class Api
     ) {
         $this->formatter = $formatter;
         $this->builder = $builder;
+    }
+
+    public function get($input)
+    {
+        $envData = $this->getEnvData();
+        return $this->exec($envData['base_url'], $envData['api_key'], $input);
+    }
+
+    public function exec($baseUrl, $apiKey, $input)
+    {
+        $client = new Client();
+        $query = $this->builder->build($input, $apiKey);
+
+        try {
+            $response = $client->request('GET', $baseUrl, $query);
+            $content = json_decode($response->getBody(), true);
+        } catch (\Throwable $th) {
+            $content = [];
+        }
+
+        $result = $this->formatter->format(
+            $content,
+            $this->getCategoryIfExists($input)
+        );
+
+        return $result;
+    }
+
+    public function getCategoryIfExists($input)
+    {
+        return isset($input['category']) ? $input['category'] : null;
     }
 }
