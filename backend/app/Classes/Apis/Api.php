914cafff6ecb4bdb627240b6e30985958b38c55b
aysar <aysarhajaj@gmail.com>
2023-02-23 17:56:35 +0200

    inject formatter and builder

--- backend/app/Classes/Apis/Api.php
+++ backend/app/Classes/Apis/Api.php
@@ -2,6 +2,19 @@
 
 namespace App\Classes\Apis;
 
+use App\Interfaces\FormatterInterface;
+use App\Interfaces\QueryBuilderInterface;
+
 class Api
 {
+    private $formatter;
+    private $builder;
+
+    public function __construct(
+        FormatterInterface $formatter,
+        QueryBuilderInterface $builder
+    ) {
+        $this->formatter = $formatter;
+        $this->builder = $builder;
+    }
 }
