c8916312a35a302c54221bfce0b2b31d6b5824e5
aysar <aysarhajaj@gmail.com>
2023-02-24 13:40:39 +0200

    add routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,57 @@
 import React from "react";
-
-import "./App.css";
+import Layout from "./root/layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import paths from "./routes/paths";
 import Login from "./features/Login";
+import RequiredAuth from "./routes/RequiredAuth";
+import Loader from "./components/Loader";
+import ROUTES from "./routes";
+import Register from "./features/Register";
+import "./App.scss";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path={paths.LOGIN} element={<Layout />}>
+          <Route path={paths.LOGIN} element={<Login />} />
+        </Route>
+        <Route path={paths.REGISTER} element={<Layout />}>
+          <Route path={paths.REGISTER} element={<Register />} />
+        </Route>
+        <Route element={<Layout />} path="/">
+          <Route element={<RequiredAuth />}>
+            <Route
+              element={
+                <React.Suspense fallback={<Loader />}>
+                  <ROUTES.HOME.ELEMENT />
+                </React.Suspense>
+              }
+              path={ROUTES.HOME.path}
+            />
+            <Route
+              element={
+                <React.Suspense fallback={<Loader />}>
+                  <ROUTES.PROFILE.ELEMENT />
+                </React.Suspense>
+              }
+              path={ROUTES.PROFILE.path}
+            />
+            <Route
+              element={
+                <React.Suspense fallback={<Loader />}>
+                  <ROUTES.SETTINGS.ELEMENT />
+                </React.Suspense>
+              }
+              path={ROUTES.SETTINGS.path}
+            />
+          </Route>
+        </Route>
+        <Route path="*" element={<Layout />}>
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
