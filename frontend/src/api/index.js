5cdcd39ad2c9c85666ec62c9660b2390157c8d8e
aysar <aysarhajaj@gmail.com>
2023-02-24 18:33:08 +0200

    add options api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,11 +3,13 @@ import api from "./axios";
 const login = (data) => api.post(`/login`, data);
 const register = (data) => api.post(`/register`, data);
 const updateProfile = (data) => api.put(`/profile`, data);
+const getSettingsOptions = () => api.get(`/settings/options`);
 
 const apis = {
   login,
   register,
   updateProfile,
+  getSettingsOptions,
 };
 
 export default apis;
