421e728ce29af440037524c366a9e9a37357b531
aysar <aysarhajaj@gmail.com>
2023-02-24 21:05:55 +0200

    add update setting cases

--- frontend/src/reducers/settingsSlice.js
+++ frontend/src/reducers/settingsSlice.js
@@ -1,10 +1,16 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../api";
+import { setSettingData } from "./authSlice";
 
 const initialState = {
   options: {
     sources: [],
     categories: [],
+    isLoading: false,
+    error: null,
+  },
+
+  update: {
     isLoading: false,
     error: null,
   },
@@ -16,6 +22,18 @@ export const getSettingsOptions = createAsyncThunk(
     api
       .getSettingsOptions(data)
       .then((response) => response.data)
+      .catch((error) => rejectWithValue(error?.response?.data))
+);
+
+export const updateSettings = createAsyncThunk(
+  "update/settings",
+  (data, { rejectWithValue, dispatch }) =>
+    api
+      .updateSettings(data.id, data)
+      .then((response) => {
+        dispatch(setSettingData(data));
+        return response.data;
+      })
       .catch((error) => rejectWithValue(error?.response?.data))
 );
 
@@ -37,10 +55,23 @@ export const settingsSlice = createSlice({
       .addCase(getSettingsOptions.rejected, (state, action) => {
         state.options.isLoading = false;
         state.options.error = action.payload.result.error;
+      })
+      // update case
+      .addCase(updateSettings.pending, (state) => {
+        state.update.isLoading = true;
+        state.update.error = null;
+      })
+      .addCase(updateSettings.fulfilled, (state, action) => {
+        state.update.isLoading = false;
+      })
+      .addCase(updateSettings.rejected, (state, action) => {
+        state.update.isLoading = false;
+        state.update.error = action.payload.result.error;
       });
   },
 });
 
 export const selectOptions = (state) => state.settings.options;
+export const selectUpdate = (state) => state.settings.update;
 
 export default settingsSlice.reducer;
