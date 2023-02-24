ccba7bd3affd6a62a3ab11902a15df1600f615eb
aysar <aysarhajaj@gmail.com>
2023-02-24 21:29:21 +0200

    add clear user and clear token functinons

--- frontend/src/storage/index.js
+++ frontend/src/storage/index.js
@@ -8,11 +8,17 @@ const setUser = (user) => localStorage.setItem(KEYS.USER, JSON.stringify(user));
 
 const setToken = (token) => localStorage.setItem(KEYS.TOKEN, token);
 
+const clearUser = () => localStorage.removeItem(KEYS.USER);
+
+const clearToken = () => localStorage.removeItem(KEYS.TOKEN);
+
 const storage = {
   getUser,
   getToken,
   setUser,
   setToken,
+  clearUser,
+  clearToken,
 };
 
 export default storage;
