0fd41a9ed41abf4bc209afd11cea587c2b5580db
aysar <aysarhajaj@gmail.com>
2023-02-24 21:04:30 +0200

    add updateSetting api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,12 +4,14 @@ const login = (data) => api.post(`/login`, data);
 const register = (data) => api.post(`/register`, data);
 const updateProfile = (data) => api.put(`/profile`, data);
 const getSettingsOptions = () => api.get(`/settings/options`);
+const updateSettings = (id, data) => api.put(`/settings/${id}`, data);
 
 const apis = {
   login,
   register,
   updateProfile,
   getSettingsOptions,
+  updateSettings,
 };
 
 export default apis;
