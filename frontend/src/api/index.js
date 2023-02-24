2f2d85d2c7a7a20d887efab4577fa26da1c38bd5
aysar <aysarhajaj@gmail.com>
2023-02-24 21:27:57 +0200

    add logout api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -5,6 +5,7 @@ const register = (data) => api.post(`/register`, data);
 const updateProfile = (data) => api.put(`/profile`, data);
 const getSettingsOptions = () => api.get(`/settings/options`);
 const updateSettings = (id, data) => api.put(`/settings/${id}`, data);
+const logout = () => api.get(`/logout`);
 
 const apis = {
   login,
@@ -12,6 +13,7 @@ const apis = {
   updateProfile,
   getSettingsOptions,
   updateSettings,
+  logout,
 };
 
 export default apis;
