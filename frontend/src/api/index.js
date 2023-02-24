0672e1ef0f0107cc1d20056c70db8c1f12457d62
aysar <aysarhajaj@gmail.com>
2023-02-24 17:43:12 +0200

    add profile api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,10 +2,12 @@ import api from "./axios";
 
 const login = (data) => api.post(`/login`, data);
 const register = (data) => api.post(`/register`, data);
+const updateProfile = (data) => api.put(`/profile`, data);
 
 const apis = {
   login,
   register,
+  updateProfile,
 };
 
 export default apis;
