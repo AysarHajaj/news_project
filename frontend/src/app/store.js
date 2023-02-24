c65eb60fc4d95a3c60bad9fd9caa9ca7c3d7ee43
aysar <aysarhajaj@gmail.com>
2023-02-24 18:32:49 +0200

    add setting slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../reducers/authSlice";
 import profileSlice from "../reducers/profileSlice";
+import settingsSlice from "../reducers/settingsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     profile: profileSlice,
+    settings: settingsSlice,
   },
 });
