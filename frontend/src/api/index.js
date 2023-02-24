b952d2b77a60a56b0506827692a6249a09d6717a
aysar <aysarhajaj@gmail.com>
2023-02-24 16:27:21 +0200

    add register function

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,11 @@
 import api from "./axios";
 
 const login = (data) => api.post(`/login`, data);
+const register = (data) => api.post(`/register`, data);
 
 const apis = {
   login,
+  register,
 };
 
 export default apis;
