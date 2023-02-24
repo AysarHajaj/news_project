54aa3a2cdc7db78afcf17f0f4e49ea2c725e85d5
aysar <aysarhajaj@gmail.com>
2023-02-24 13:40:18 +0200

    add required auth component

---
+++ frontend/src/routes/RequiredAuth.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import paths from "./paths";
+
+function RequireAuth() {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+  console.log(isAuthenticated);
+  if (!isAuthenticated)
+    return <Navigate to={paths.LOGIN} state={{ from: location }} replace />;
+
+  return <Outlet />;
+}
+
+export default RequireAuth;
